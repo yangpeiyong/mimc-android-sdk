30fa3399b20363cf3353e1c74e3f8a27a2867de6
houminjiang <houminjiang@xiaomi.com>
2018-01-05 18:03:03 +0800

    Update SDK

--- demo/app/src/com/xiaomi/mimcdemo/Constant.java
+++
@@ -1,5 +0,0 @@
-package com.xiaomi.mimcdemo;
-
-public class Constant {
-    public static final String ACTION = "com.xiaomi.xmsevicedemo.LOCAL_RECEIVE";
-}
--- sdk/changelog.md
+++ sdk/changelog.md
@@ -1,3 +1,5 @@
+# 1.0.9
++ fix bugs
 # 1.0.8
 + support to get new token
 # 1.0.7
